c6819c2d83ef7302e8eeb3c229f0933b83de34b8
diego_neri <diego@diegoneri.com>
2020-06-18 19:16:30 -0300

    Cria o novo projeto console donet core

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PesoIdeal_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
